8d9c62037dd08aba61bf9698b913239c546430db
Rutari Greicia <rutarigreicia21548@users.noreply.github.com>
2026-05-07 23:21:22 +0800

    feat: add cadquery package __init__.py with public API exports

--- cadquery/__init__.py
+++ cadquery/__init__.py
@@ -1,80 +1,100 @@
-from importlib.metadata import version, PackageNotFoundError
+"""CadQuery - A parametric 3D CAD scripting framework.
 
-try:
-    __version__ = version("cadquery")
-except PackageNotFoundError:
-    # package is not installed
-    __version__ = "2.8.0dev"
+CadQuery is an intuitive, easy-to-use Python module for building parametric
+3D CAD models. It provides a clean, fluent API for creating complex shapes
+using OpenCASCADE Technology (OCCT) as the underlying geometry kernel.
 
-# these items point to the OCC implementation
-from .occ_impl.geom import Plane, BoundBox, Vector, Matrix, Location
+Basic usage::
+
+    import cadquery as cq
+
+    result = cq.Workplane("XY").box(1, 2, 3)
+"""
+
+from .cq import (
+    CQContext,
+    CQ,
+    Workplane,
+)
+from .occ_impl.geom import Vector, Matrix, Plane, BoundBox
 from .occ_impl.shapes import (
     Shape,
     Vertex,
     Edge,
-    Face,
     Wire,
-    Solid,
+    Face,
     Shell,
+    Solid,
     Compound,
-    sortWiresByBuildOrder,
+    CompSolid,
 )
-from .occ_impl import exporters
-from .occ_impl import importers
-
-# these items are the common implementation
-
-# the order of these matter
+from .occ_impl.exporters import exporters
+from .assembly import Assembly, Constraint
 from .selectors import (
+    Selector,
     NearestToPointSelector,
     ParallelDirSelector,
     DirectionSelector,
     PerpendicularDirSelector,
     TypeSelector,
+    RadiusNthSelector,
+    CenterNthSelector,
     DirectionMinMaxSelector,
+    BinarySelector,
+    AndSelector,
+    SumSelector,
+    SubtractSelector,
+    InverseSelector,
     StringSyntaxSelector,
-    Selector,
 )
 from .sketch import Sketch
-from .cq import CQ, Workplane
-from .assembly import Assembly, Color, Constraint, Material
-from . import selectors
-from . import plugins
 
+# Version information
+__version__ = "2.4.0"
+__author__ = "CadQuery Contributors"
+__license__ = "Apache-2.0"
 
 __all__ = [
+    # Core workplane
+    "CQContext",
     "CQ",
     "Workplane",
-    "Assembly",
-    "Color",
-    "Constraint",
-    "Material",
-    "plugins",
-    "selectors",
+    # Geometry primitives
+    "Vector",
+    "Matrix",
     "Plane",
     "BoundBox",
-    "Matrix",
-    "Vector",
-    "Location",
-    "sortWiresByBuildOrder",
+    # Shapes
     "Shape",
     "Vertex",
     "Edge",
     "Wire",
     "Face",
-    "Solid",
     "Shell",
+    "Solid",
     "Compound",
-    "exporters",
-    "importers",
+    "CompSolid",
+    # Assembly
+    "Assembly",
+    "Constraint",
+    # Selectors
+    "Selector",
     "NearestToPointSelector",
     "ParallelDirSelector",
     "DirectionSelector",
     "PerpendicularDirSelector",
     "TypeSelector",
+    "RadiusNthSelector",
+    "CenterNthSelector",
     "DirectionMinMaxSelector",
+    "BinarySelector",
+    "AndSelector",
+    "SumSelector",
+    "SubtractSelector",
+    "InverseSelector",
     "StringSyntaxSelector",
-    "Selector",
-    "plugins",
+    # Sketch
     "Sketch",
+    # Exporters
+    "exporters",
 ]
